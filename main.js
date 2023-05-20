6f0d0e963fb16a66a86cccf37d01c077c61a6646
Parsa Khosravani <parsakhosravani@gmail.com>
2023-05-20 16:28:07 +0330

    add increment async

--- main.js
+++ main.js
@@ -1,25 +1,28 @@
-import "@babel/polyfill"
+import "@babel/polyfill";
 
-import React from 'react'
-import ReactDOM from 'react-dom'
-import { createStore, applyMiddleware } from 'redux'
+import React from "react";
+import ReactDOM from "react-dom";
+import { createStore, applyMiddleware } from "redux";
+import { helloSaga } from "./sagas";
+import Counter from "./Counter";
+import reducer from "./reducers";
+import createSagaMiddleware from "redux-saga";
 
-import Counter from './Counter'
-import reducer from './reducers'
-
-const store = createStore(reducer)
-
-const action = type => store.dispatch({type})
+const store = createStore(reducer, applyMiddleware(sagaMiddleware));
+const sagaMiddleware = createSagaMiddleware();
+sagaMiddleware.run(helloSaga);
+const action = (type) => store.dispatch({ type });
 
 function render() {
   ReactDOM.render(
     <Counter
       value={store.getState()}
-      onIncrement={() => action('INCREMENT')}
-      onDecrement={() => action('DECREMENT')} />,
-    document.getElementById('root')
-  )
+      onIncrement={() => action("INCREMENT")}
+      onDecrement={() => action("DECREMENT")}
+      onIncrementAsync={() => action("INCREMENT_ASYNC")}
+    />,
+    document.getElementById("root")
+  );
 }
-
-render()
-store.subscribe(render)
+render();
+store.subscribe(render);
